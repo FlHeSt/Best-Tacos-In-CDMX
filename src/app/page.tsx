05836d1cd586fb7722b6733af1952296ec1a5f3a
Florian Köhler <flokoehler99@gmail.com>
2024-11-04 14:14:07 +0100

    fixed orientation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Orientation from "./components/Orientation";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <h1>Hello World</h1>
+      <Orientation />
+      <p>This is a paragraph</p>
       <Footer />
     </div>
   );
