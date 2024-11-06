9556ae0f3ab305354afbd1de31599fef471d66fe
Florian Köhler <flokoehler99@gmail.com>
2024-11-06 23:31:27 +0100

    add green divs

---
+++ src/app/components/green divs.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const GreenDivs = () => {
+  return (
+    <div className="flex justify-around mt-4">
+      <div className="bg-green-500 w-1/3 h-32">Green Div</div>
+      <div className="bg-green-500 w-1/3 h-32">Green Div</div>
+      <div className="bg-green-500 w-1/3 h-32">Green Div</div>
+    </div>
+  );
+};
+
+export default GreenDivs;
