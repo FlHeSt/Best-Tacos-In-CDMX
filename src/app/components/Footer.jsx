0fb66cbf7d1dedb0e3ea8e57f70c996d29b18f4e
Florian Köhler <flokoehler99@gmail.com>
2024-11-06 18:28:52 +0100

    make year dynamic

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,16 +1,24 @@
 const Footer = () => {
-    return (
-      <footer className="bg-gray-800 text-white p-4 mt-auto">
-        <div className="container mx-auto text-center">
-          <p>© 2024 CDMX Taco Finder. All rights reserved.</p>
-          <div className="mt-2">
-            <a href="#" className="hover:text-yellow-500 mx-2">Instagram</a>
-            <a href="#" className="hover:text-yellow-500 mx-2">Facebook</a>
-            <a href="#" className="hover:text-yellow-500 mx-2">Twitter</a>
-          </div>
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-auto">
+      <div className="container mx-auto text-center">
+        <p>
+          © {new Date().getFullYear()} CDMX Taco Finder. All rights reserved.
+        </p>
+        <div className="mt-2">
+          <a href="#" className="hover:text-yellow-500 mx-2">
+            Instagram
+          </a>
+          <a href="#" className="hover:text-yellow-500 mx-2">
+            Facebook
+          </a>
+          <a href="#" className="hover:text-yellow-500 mx-2">
+            Twitter
+          </a>
         </div>
-      </footer>
-    );
-  };
-  
-  export default Footer;
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
